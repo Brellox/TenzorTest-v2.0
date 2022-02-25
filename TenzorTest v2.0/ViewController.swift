0e83b1fc6d1d6fe02fbe45477db7c5650e85d964
Иван <vanchous3000@gmail.com>
2022-02-25 15:12:55 +0700

    Initial Commit

---
+++ TenzorTest v2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TenzorTest v2.0
+//
+//  Created by Иван Суслов on 25.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
